faff7fb303debc2e9d8bd839238becc838a62ffe
EnigmaCurry <ryan@enigmacurry.com>
2024-07-07 14:10:24 -0600

    hello state

--- server/src/api/test/hello.rs
+++ server/src/api/test/hello.rs
@@ -1,10 +1,9 @@
+use super::test_route;
 use crate::SharedState;
 use axum::body::Bytes;
 use axum::extract::{Path, State};
 use axum::{routing::get, routing::MethodRouter, Router};
-use text_sanitizer::TextSanitizer;
-
-use super::test_route;
+use regex::Regex;
 
 pub fn main() -> Router<SharedState> {
     Router::new().merge(hello()).merge(hello_name())
@@ -21,25 +20,28 @@ fn hello() -> Router<SharedState> {
         let default_bytes = Bytes::from(default.as_bytes());
         let name = std::str::from_utf8(cache.get("test::hello::name").unwrap_or(&default_bytes))
             .unwrap_or(&default);
-        format!("Hello, {name}!\n")
+        if name == default {
+            format!("Hello, {default}!")
+        } else {
+            format!("Hello! The last one here was {name}!\n")
+        }
     }
     route("/", get(handler))
 }
 
 fn hello_name() -> Router<SharedState> {
     async fn handler(Path(name): Path<String>, State(state): State<SharedState>) -> String {
-        let mut sanitizer = TextSanitizer::new_with_options(false, true, false);
-        sanitizer.add_request_language(&"en");
-        let name: Bytes = Bytes::from(sanitizer.sanitize_u8(name.as_bytes()));
-        state
-            .write()
-            .unwrap()
-            .cache
-            .insert("test::hello::name".to_string(), name.clone());
-        format!(
-            "Hello, {}!\n",
-            std::str::from_utf8(&name).unwrap_or("World")
-        )
+        let re = Regex::new(r"^[a-zA-Z][a-zA-Z0-9]+$").unwrap();
+        if re.is_match(&name) {
+            state
+                .write()
+                .unwrap()
+                .cache
+                .insert("test::hello::name".to_string(), Bytes::from(name.clone()));
+            format!("Hello, {}!\n", name)
+        } else {
+            format!("Sorry, names must be alphanumeric only.")
+        }
     }
     route("/:name", get(handler))
 }
