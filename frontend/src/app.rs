af4ae62add1a9d4993a89df6396ea731f51a4e0c
EnigmaCurry <ryan@enigmacurry.com>
2024-07-29 20:23:18 -0600

    closed sidebar.

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -28,11 +28,18 @@ pub fn app() -> Html {
         })
     };
 
+    let close_sidebar = {
+        let sidebar_open = sidebar_open.clone();
+        Callback::from(move |_| {
+            sidebar_open.set(false);
+        })
+    };
+
     html! {
         <BackdropViewer>
           <ToastViewer>
             <Router<AppRoute> default={AppRoute::Index}>
-              <RouterSwitch<AppRoute> render={move |target| switch_app_route(target, close_sidebar.clone(), sidebar_open.clone())} />
+              <RouterSwitch<AppRoute> render={move |target| switch_app_route(target, sidebar_open.clone(), close_sidebar.clone())} />
             </Router<AppRoute>>
           </ToastViewer>
         </BackdropViewer>
@@ -41,12 +48,12 @@ pub fn app() -> Html {
 
 fn switch_app_route(
     target: AppRoute,
-    close_sidebar: Callback<MouseEvent>,
     sidebar_open: UseStateHandle<bool>,
+    close_sidebar: Callback<()>,
 ) -> Html {
     match target {
         AppRoute::Index => {
-            html! {<AppPage close_sidebar={close_sidebar} sidebar_open={sidebar_open}><Index/></AppPage>}
+            html! {<AppPage sidebar_open={sidebar_open} close_sidebar={close_sidebar}><Index/></AppPage>}
         }
     }
 }
@@ -54,38 +61,32 @@ fn switch_app_route(
 #[derive(Clone, Debug, PartialEq, Properties)]
 pub struct PageProps {
     pub children: Children,
-    pub close_sidebar: Callback<MouseEvent>,
     pub sidebar_open: UseStateHandle<bool>,
+    pub close_sidebar: Callback<()>,
 }
 
 #[function_component(AppPage)]
 fn page(props: &PageProps) -> Html {
     let tools = html! { "tools!" };
     let brand = html! { "brand!" };
+
     let sidebar = html_nested! {
-        <PageSidebar open={false}>
-          <Nav>
-            <NavList>
-              <NavExpandable title="Navigation">
-                // <NavItem>
-                //   <a href="#" onclick={props.close_sidebar.reform(|e: MouseEvent| {
-                //       e.prevent_default();
-                //       e
-                //   })}>
-                    <NavRouterItem<AppRoute> to={AppRoute::Index}>
-                      {"Index"}
-                    </NavRouterItem<AppRoute>>
-                //   </a>
-                // </NavItem>
-              </NavExpandable>
-            </NavList>
-          </Nav>
+        <PageSidebar>
+            <Nav>
+                <NavList>
+                    <NavExpandable>
+                        <NavRouterItem<AppRoute> to={AppRoute::Index}>
+                            {"Index"}
+                        </NavRouterItem<AppRoute>>
+                    </NavExpandable>
+                </NavList>
+            </Nav>
         </PageSidebar>
     };
 
-    html! (
-        <Page {tools} {brand} {sidebar}>
+    html! {
+        <Page {tools} {brand} {sidebar} open={*props.sidebar_open}>
             { for props.children.iter() }
         </Page>
-    )
+    }
 }
