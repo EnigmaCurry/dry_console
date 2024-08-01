0f4887f87b60ab02fa12f012db83b48e40fec909
Ryan McGuire <ryan@enigmacurry.com>
2024-07-31 20:53:51 -0600

    fix: build (#27)
    * build
    
    * fix: build
    
    * clippy

--- server/build.rs
+++ server/build.rs
@@ -1,17 +1,30 @@
 use std::env;
 use std::fs::File;
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
 fn main() {
     // Get the output directory from the environment variable
     let out_dir = env::var("OUT_DIR").unwrap();
+    let project_root: String = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+        .parent()
+        .unwrap()
+        .as_os_str()
+        .to_string_lossy()
+        .to_string();
     let dest_path = Path::new(&out_dir).join("generated_includes.rs");
     let mut file = File::create(dest_path).unwrap();
 
-    // Define the path to the directory containing the snippets
-    let dist_path = Path::new("../dist/snippets");
+    // Define the path to the directory containing assets
+    let dist_dir = PathBuf::from(project_root.clone())
+        .join("dist")
+        .to_string_lossy()
+        .to_string();
+    let snippets_dir = PathBuf::from(&dist_dir)
+        .join("snippets")
+        .to_string_lossy()
+        .to_string();
 
     // Write the start of the function definition to the generated file
     writeln!(
@@ -22,23 +35,26 @@ fn main() {
     writeln!(file, "    vec![").unwrap();
 
     // Walk through the ../dist/snippets directory and find all inline0.js files
-    for entry in WalkDir::new(dist_path).into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new(snippets_dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
         if entry.path().is_file()
             && entry
                 .file_name()
                 .to_str()
                 .map_or(false, |f| f.ends_with(".js"))
         {
-            let relative_path = entry.path().strip_prefix("..").unwrap();
+            let asset_path = entry.path();
 
             writeln!(
                 file,
-                "        (\"{}\", include_bytes!(\"../../../../../../{}\"), \"application/javascript\"),",
-                relative_path
+                "        (\"{}\", include_bytes!(\"{}\"), \"application/javascript\"),",
+                asset_path
                     .to_string_lossy()
-                    .strip_prefix("dist")
+                    .strip_prefix(&dist_dir.to_string())
                     .unwrap(),
-                relative_path.to_string_lossy()
+                asset_path.to_string_lossy()
             )
             .unwrap();
         }
@@ -47,7 +63,7 @@ fn main() {
     // App CSS
     writeln!(
         file,
-        "        (\"/style.css\", include_bytes!(\"../../../../../../frontend/style.css\"), \"text/css\"),",
+        "        (\"/style.css\", include_bytes!(\"{project_root}/frontend/style.css\"), \"text/css\"),"
     )
     .unwrap();
 
@@ -55,24 +71,24 @@ fn main() {
     // TODO: Tree shake this 1.5MB
     writeln!(
         file,
-        "        (\"/patternfly.min.css\", include_bytes!(\"../../../../../../frontend/node_modules/@patternfly/patternfly/patternfly.min.css\"), \"text/css\"),",
+        "        (\"/patternfly.min.css\", include_bytes!(\"{dist_dir}/patternfly.min.css\"), \"text/css\"),",
     )
     .unwrap();
     writeln!(
         file,
-        "        (\"/patternfly.min.css.map\", include_bytes!(\"../../../../../../frontend/node_modules/@patternfly/patternfly/patternfly.min.css.map\"), \"application/octet-stream\"),",
+        "        (\"/patternfly.min.css.map\", include_bytes!(\"{dist_dir}/patternfly.min.css.map\"), \"application/octet-stream\"),",
     )
     .unwrap();
 
     // Patternfly fonts
     writeln!(
         file,
-        "        (\"/assets/fonts/webfonts/fa-solid-900.woff2\", include_bytes!(\"../../../../../../frontend/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2\"), \"font/woff2\"),",
+        "        (\"/assets/fonts/webfonts/fa-solid-900.woff2\", include_bytes!(\"{dist_dir}/assets/fonts/webfonts/fa-solid-900.woff2\"), \"font/woff2\"),",
     )
     .unwrap();
     writeln!(
         file,
-        "        (\"/assets/fonts/RedHatText/RedHatText-Regular.woff2\", include_bytes!(\"../../../../../../frontend/node_modules/@patternfly/patternfly/assets/fonts/RedHatText/RedHatText-Regular.woff2\"), \"font/woff2\"),",
+        "        (\"/assets/fonts/RedHatText/RedHatText-Regular.woff2\", include_bytes!(\"{dist_dir}/assets/fonts/RedHatText/RedHatText-Regular.woff2\"), \"font/woff2\"),",
     )
     .unwrap();
 
