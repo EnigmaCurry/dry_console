23acf9ec1cf4a4e5389b60c642db084fb17c770a
EnigmaCurry <ryan@enigmacurry.com>
2024-08-07 00:02:56 -0600

    deps

--- server/src/api/workstation.rs
+++ server/src/api/workstation.rs
@@ -8,8 +8,18 @@ use strum::{AsRefStr, EnumIter, EnumProperty, EnumString, IntoEnumIterator};
 use utoipa::ToSchema;
 use which::which;
 
+mod bash;
+mod curl;
 mod docker;
 mod git;
+mod htpasswd;
+mod jq;
+mod make;
+mod openssl;
+mod sed;
+mod shred;
+mod xargs;
+mod xdg_open;
 
 pub fn router() -> Router<SharedState> {
     Router::new()
@@ -44,11 +54,33 @@ pub enum WorkstationDependencies {
     git,
     #[strum(props(Version = "*"))]
     docker,
+    #[strum(props(Version = "*"))]
+    bash,
+    #[strum(props(Version = "*"))]
+    make,
+    #[strum(props(Version = "*"))]
+    sed,
+    #[strum(props(Version = "*"))]
+    xargs,
+    #[strum(props(Version = "*"))]
+    shred,
+    #[strum(props(Version = "*"))]
+    openssl,
+    #[strum(props(Version = "*"))]
+    htpasswd,
+    #[strum(props(Version = "*"))]
+    jq,
+    #[strum(props(Version = "*", Name = "xdg-open"))]
+    xdg_open,
+    #[strum(props(Version = "*"))]
+    curl,
 }
 impl WorkstationDependencies {
     fn get_version(&self) -> VersionReq {
-        // Retrieve the version property using strum's get_str method
-        VersionReq::parse(self.get_str("Version").unwrap()).unwrap()
+        VersionReq::parse(self.get_str("Version").unwrap_or("*")).unwrap()
+    }
+    fn get_name(&self) -> &str {
+        self.get_str("Name").unwrap_or_else(|| self.as_ref())
     }
 }
 
@@ -100,7 +132,7 @@ fn required_dependencies() -> Router<SharedState> {
     async fn handler() -> impl IntoResponse {
         let dependencies: Vec<WorkstationDependencyInfo> = WorkstationDependencies::iter()
             .map(|dep| WorkstationDependencyInfo {
-                name: dep.as_ref().to_string(),
+                name: dep.get_name().to_string(),
                 version: dep.get_version().to_string(),
             })
             .collect();
@@ -121,12 +153,12 @@ fn required_dependencies() -> Router<SharedState> {
 )]
 fn dependencies() -> Router<SharedState> {
     async fn handler(Path(name): Path<String>) -> impl IntoResponse {
-        match WorkstationDependencies::from_str(&name.clone()).ok() {
+        match WorkstationDependencies::from_str(&name.clone().replace("-", "_")).ok() {
             Some(dependency) => {
                 // Check if dependency is installed:
                 let mut installed = false;
                 let mut version = String::new();
-                let path = match which(OsStr::new(dependency.as_ref())) {
+                let path = match which(OsStr::new(dependency.get_name())) {
                     Ok(p) => {
                         installed = true;
                         p.to_string_lossy().to_string()
@@ -135,12 +167,52 @@ fn dependencies() -> Router<SharedState> {
                 };
                 if installed {
                     match dependency {
+                        WorkstationDependencies::git => {
+                            let v = git::get_version();
+                            version = v;
+                        }
                         WorkstationDependencies::docker => {
                             let v = docker::get_version();
                             version = v;
                         }
-                        WorkstationDependencies::git => {
-                            let v = git::get_version();
+                        WorkstationDependencies::bash => {
+                            let v = bash::get_version();
+                            version = v;
+                        }
+                        WorkstationDependencies::make => {
+                            let v = make::get_version();
+                            version = v;
+                        }
+                        WorkstationDependencies::sed => {
+                            let v = sed::get_version();
+                            version = v;
+                        }
+                        WorkstationDependencies::xargs => {
+                            let v = xargs::get_version();
+                            version = v;
+                        }
+                        WorkstationDependencies::shred => {
+                            let v = shred::get_version();
+                            version = v;
+                        }
+                        WorkstationDependencies::openssl => {
+                            let v = openssl::get_version();
+                            version = v;
+                        }
+                        WorkstationDependencies::htpasswd => {
+                            let v = htpasswd::get_version();
+                            version = v;
+                        }
+                        WorkstationDependencies::jq => {
+                            let v = jq::get_version();
+                            version = v;
+                        }
+                        WorkstationDependencies::xdg_open => {
+                            let v = xdg_open::get_version();
+                            version = v;
+                        }
+                        WorkstationDependencies::curl => {
+                            let v = curl::get_version();
                             version = v;
                         }
                     }
