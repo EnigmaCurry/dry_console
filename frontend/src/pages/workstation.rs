fc80a69e4edbc430df36af390ff4bc35a65bf178
EnigmaCurry <ryan@enigmacurry.com>
2024-08-07 02:15:44 -0600

    welcome

--- frontend/src/pages/workstation.rs
+++ frontend/src/pages/workstation.rs
@@ -1,10 +1,15 @@
+use gloo_events::EventListener;
+use gloo_utils::window;
 use patternfly_yew::prelude::*;
+use std::str::FromStr;
+use strum_macros::{AsRefStr, EnumString};
 use yew::prelude::*;
 
 mod dependencies;
 mod system;
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq, EnumString, AsRefStr)]
+#[strum(serialize_all = "kebab-case")]
 enum WorkstationTab {
     System,
     Dependencies,
@@ -12,15 +17,41 @@ enum WorkstationTab {
 
 #[function_component(WorkstationTabs)]
 fn tabs() -> Html {
-    let selected = use_state_eq(|| WorkstationTab::System);
+    let selected = use_state_eq(|| {
+        let location = window().location();
+        let hash = location.hash().unwrap_or_default();
+        WorkstationTab::from_str(&hash.trim_start_matches('#')).unwrap_or(WorkstationTab::System)
+    });
+
     let reload_trigger = use_state(|| 0);
+
+    {
+        let selected = selected.clone();
+        let reload_trigger = reload_trigger.clone();
+        use_effect(move || {
+            let window = window();
+            let listener = EventListener::new(&window.clone(), "hashchange", move |_event| {
+                let location = window.location();
+                let hash = location.hash().unwrap_or_default();
+                if let Ok(tab) = WorkstationTab::from_str(&hash.trim_start_matches('#')) {
+                    if tab == WorkstationTab::Dependencies {
+                        reload_trigger.set(*reload_trigger + 1); // Trigger reload when Dependencies tab is selected via hash change
+                    }
+                    selected.set(tab);
+                }
+            });
+            listener.forget(); // Forget the listener to keep it active
+            || ()
+        });
+    }
 
     let onselect = {
         let selected = selected.clone();
         let reload_trigger = reload_trigger.clone();
         Callback::from(move |index: WorkstationTab| {
+            window().location().set_hash(index.as_ref()).unwrap();
             if index == WorkstationTab::Dependencies {
-                reload_trigger.set(*reload_trigger + 1); // Trigger reload when Dependencies tab is selected
+                reload_trigger.set(*reload_trigger + 1);
             }
             selected.set(index);
         })
@@ -28,7 +59,7 @@ fn tabs() -> Html {
 
     html! (
         <>
-            <Tabs<WorkstationTab> detached=true {onselect} selected={*selected} r#box=true>
+            <Tabs<WorkstationTab> detached=true {onselect} selected={(*selected).clone()} r#box=true>
                 <Tab<WorkstationTab> index={WorkstationTab::System} title="System"/>
                 <Tab<WorkstationTab> index={WorkstationTab::Dependencies} title="Dependencies"/>
             </Tabs<WorkstationTab>>
--- frontend/src/pages/workstation/system.rs
+++ frontend/src/pages/workstation/system.rs
@@ -1,15 +1,17 @@
-use gloo::net::http::Request;
 use patternfly_yew::prelude::*;
-use serde::Deserialize;
-use yew::platform::spawn_local;
 use yew::prelude::*;
-use yew::virtual_dom::VChild;
 
 #[function_component(System)]
 pub fn system() -> Html {
     html! {
-        <>
-        {" System "}
-        </>
+    <Card>
+            <CardTitle><h1>{"🖖 Welcome"}</h1></CardTitle>
+            <CardBody>
+            {"Todo items:"}
+            <ol>
+            <li><a href="/workstation#dependencies">{"Install dependencies"}</a></li>
+            </ol>
+            </CardBody>
+            </Card>
     }
 }
