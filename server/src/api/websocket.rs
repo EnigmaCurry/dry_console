a64fc9bfa5b5e79004cfd3b5dd3058ff9cd602f2
EnigmaCurry <ryan@enigmacurry.com>
2024-08-14 13:29:59 -0600

    websocket

--- server/src/api/websocket.rs
+++ server/src/api/websocket.rs
@@ -5,6 +5,8 @@ use dry_console_dto::websocket::CloseCode;
 use serde::{Deserialize, Serialize};
 use std::future::Future;
 use std::pin::Pin;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 use tracing::*;
 
@@ -15,43 +17,53 @@ pub struct WebSocketResponse {
 }
 
 pub async fn handle_websocket<T, U, F>(
-    mut socket: WebSocket<T, U>,
+    socket: Arc<Mutex<Option<WebSocket<T, U>>>>,
     mut shutdown: broadcast::Receiver<()>,
     mut on_message: F,
 ) where
-    T: Serialize + for<'de> Deserialize<'de>,
-    U: Serialize + for<'de> Deserialize<'de>,
+    T: Serialize + for<'de> Deserialize<'de> + 'static,
+    U: Serialize + for<'de> Deserialize<'de> + 'static,
     F: FnMut(Message<U>) -> Pin<Box<dyn Future<Output = Option<WebSocketResponse>> + Send>>,
 {
     let mut close_code: Option<CloseCode> = None;
     let mut close_message: Option<String> = None;
 
     loop {
-        tokio::select! {
-            Some(msg) = socket.recv() => {
-                match msg {
-                    Ok(item) => {
-                        if let Some(response) = on_message(item).await {
-                            close_code = Some(response.close_code);
-                            close_message = Some(response.close_message);
-                            if response.close {
-                                break;
-                            }
-                        }
-                    },
-                    Err(err) => {
+        let msg = {
+            let mut socket_guard = socket.lock().await;
+            if let Some(socket) = socket_guard.as_mut() {
+                match socket.recv().await {
+                    Some(Ok(item)) => Some(item),
+                    Some(Err(err)) => {
                         close_code = Some(CloseCode::InvalidFramePayloadData);
-                        close_message = Some("Error parsing message.".to_string());
-                        error!("Got error: {}", err);
-                        break;
+                        close_message = Some(format!("Error parsing message: {}", err));
+                        None
                     }
+                    _ => None, // WebSocket closed
+                }
+            } else {
+                None
+            }
+        };
+
+        if let Some(item) = msg {
+            if let Some(response) = on_message(item).await {
+                close_code = Some(response.close_code);
+                close_message = Some(response.close_message);
+                if response.close {
+                    break;
                 }
             }
-            _ = shutdown.recv() => {
-                close_code = Some(CloseCode::GoingAway);
-                close_message = Some("Server is shutting down.".to_string());
-                break;
-            }
+        } else {
+            close_code = Some(CloseCode::NormalClosure);
+            close_message = Some("Connection closed by client.".to_string());
+            break;
+        }
+
+        if shutdown.recv().await.is_ok() {
+            close_code = Some(CloseCode::GoingAway);
+            close_message = Some("Server is shutting down.".to_string());
+            break;
         }
 
         if close_code.is_some() {
@@ -66,12 +78,18 @@ pub async fn handle_websocket<T, U, F>(
             .unwrap_or_else(|| "Goodbye.".to_string())
             .into(),
     };
-    if let Err(err) = socket.send(Message::Close(Some(close_frame))).await {
-        eprintln!("Error sending close frame: {:?}", err);
+
+    // Take ownership of the WebSocket and close it
+    let mut socket_guard = socket.lock().await;
+    if let Some(mut socket_owned) = socket_guard.take() {
+        if let Err(err) = socket_owned.send(Message::Close(Some(close_frame))).await {
+            eprintln!("Error sending close frame: {:?}", err);
+        }
+
+        if let Err(err) = socket_owned.close().await {
+            eprintln!("Error closing the socket: {:?}", err);
+        }
     }
 
-    if let Err(err) = socket.close().await {
-        eprintln!("Error closing the socket: {:?}", err);
-    }
     info!("Closed websocket.");
 }
--- server/src/api/workstation/command_execute.rs
+++ server/src/api/workstation/command_execute.rs
@@ -1,5 +1,5 @@
 use std::process::Stdio;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use crate::api::websocket::{handle_websocket, WebSocketResponse};
 use crate::broadcast;
@@ -7,10 +7,14 @@ use crate::{api::route, AppRouter};
 use axum::{response::IntoResponse, routing::get, Router};
 use axum_typed_websockets::{Message, WebSocket, WebSocketUpgrade};
 use dry_console_dto::websocket::{ClientMsg, CloseCode, ProcessComplete, ProcessOutput, ServerMsg};
+use tokio::io::AsyncBufReadExt;
 use tokio::io::BufReader;
 use tokio::process::Command;
+use tokio::sync::Mutex;
 use tokio::time::Instant;
+use tokio_stream::StreamExt;
 use tracing::debug;
+use ulid::Ulid;
 
 pub fn main(shutdown: broadcast::Sender<()>) -> AppRouter {
     Router::new().merge(command_execute(shutdown))
@@ -31,76 +35,134 @@ fn command_execute(shutdown: broadcast::Sender<()>) -> AppRouter {
         RunningProcess,
         Completed,
     }
-    /// state machine websocket is spawned once per connection:
-    async fn websocket(
-        mut socket: WebSocket<ServerMsg, ClientMsg>,
-        mut shutdown: broadcast::Receiver<()>,
-    ) {
-        let start = Instant::now();
+
+    /// WebSocket connection handler
+    async fn websocket(socket: WebSocket<ServerMsg, ClientMsg>, shutdown: broadcast::Receiver<()>) {
         let state = Arc::new(Mutex::new(State::AwaitingPong));
+        let socket = Arc::new(Mutex::new(Some(socket)));
 
-        socket.send(Message::Item(ServerMsg::Ping)).await.ok();
+        {
+            // Initial Ping
+            let mut socket_guard = socket.lock().await;
+            socket_guard
+                .as_mut()
+                .unwrap()
+                .send(Message::Item(ServerMsg::Ping))
+                .await
+                .ok();
+        }
 
-        handle_websocket(socket, shutdown, move |msg| {
+        handle_websocket(socket.clone(), shutdown, move |msg| {
             let state = state.clone();
-            Box::pin({
-                async move {
-                    let mut state_ref = state.lock().unwrap();
-                    match *state_ref {
-                        State::AwaitingPong => match msg {
-                            Message::Item(ClientMsg::Pong) => {
-                                *state_ref = State::AwaitingCommand;
-                                Some(WebSocketResponse {
-                                    close: false,
-                                    close_code: CloseCode::NormalClosure,
-                                    close_message: "Awaiting command".to_string(),
-                                })
-                            }
-                            _ => Some(WebSocketResponse {
-                                close: true,
-                                close_code: CloseCode::UnsupportedData,
-                                close_message: "Received unexpected message.".to_string(),
-                            }),
-                        },
-                        State::AwaitingCommand => match msg {
-                            Message::Item(ClientMsg::Command(_command_id)) => {
-                                let process_id = ulid::Ulid::new();
-                                *state_ref = State::RunningProcess;
-                                // Run the command asynchronously
-                                // Simulate long running process here if needed
-                                Some(WebSocketResponse {
-                                    close: false,
-                                    close_code: CloseCode::NormalClosure,
-                                    close_message: "Running process".to_string(),
-                                })
-                            }
-                            _ => Some(WebSocketResponse {
-                                close: true,
-                                close_code: CloseCode::UnsupportedData,
-                                close_message: "Received unexpected message.".to_string(),
-                            }),
-                        },
-                        State::RunningProcess | State::Completed => Some(WebSocketResponse {
+            let socket = socket.clone();
+            Box::pin(async move {
+                let mut state_ref = state.lock().await;
+                let mut socket_ref = socket.lock().await;
+                let socket_guard = socket_ref.as_mut().unwrap();
+
+                match *state_ref {
+                    State::AwaitingPong => match msg {
+                        Message::Item(ClientMsg::Pong) => {
+                            *state_ref = State::AwaitingCommand;
+                            Some(WebSocketResponse {
+                                close: false,
+                                close_code: CloseCode::NormalClosure,
+                                close_message: "Awaiting command".to_string(),
+                            })
+                        }
+                        _ => Some(WebSocketResponse {
                             close: true,
                             close_code: CloseCode::UnsupportedData,
                             close_message: "Received unexpected message.".to_string(),
                         }),
-                    }
+                    },
+                    State::AwaitingCommand => match msg {
+                        Message::Item(ClientMsg::Command(_command_id)) => {
+                            *state_ref = State::RunningProcess;
+                            drop(state_ref); // Drop the lock on state to run the command
+
+                            // Run the command asynchronously
+                            let process_id = Ulid::new();
+                            let _start = Instant::now();
+                            let mut process = Command::new("seq")
+                                .arg("10")
+                                .stdout(Stdio::piped())
+                                .spawn()
+                                .expect("Failed to start process");
+
+                            // Handle the stdout
+                            if let Some(stdout) = process.stdout.take() {
+                                let reader = BufReader::new(stdout);
+                                let mut reader_stream =
+                                    tokio_stream::wrappers::LinesStream::new(reader.lines());
+
+                                while let Some(line_result) = reader_stream.next().await {
+                                    match line_result {
+                                        Ok(line_content) => {
+                                            socket_guard
+                                                .send(Message::Item(ServerMsg::ProcessOutput(
+                                                    ProcessOutput {
+                                                        id: process_id,
+                                                        line: line_content,
+                                                    },
+                                                )))
+                                                .await
+                                                .ok();
+                                        }
+                                        Err(e) => {
+                                            eprintln!("Error reading line: {}", e);
+                                        }
+                                    }
+                                }
+                            }
+
+                            // Wait for the process to finish
+                            let status = process
+                                .wait()
+                                .await
+                                .expect("Failed to wait on child process");
+                            socket_guard
+                                .send(Message::Item(ServerMsg::ProcessComplete(ProcessComplete {
+                                    id: process_id,
+                                    code: status.code().unwrap_or(128),
+                                })))
+                                .await
+                                .ok();
+
+                            *state.lock().await = State::Completed; // Update the state
+                            Some(WebSocketResponse {
+                                close: false,
+                                close_code: CloseCode::NormalClosure,
+                                close_message: "Running process".to_string(),
+                            })
+                        }
+                        _ => Some(WebSocketResponse {
+                            close: true,
+                            close_code: CloseCode::UnsupportedData,
+                            close_message: "Received unexpected message.".to_string(),
+                        }),
+                    },
+                    State::RunningProcess | State::Completed => Some(WebSocketResponse {
+                        close: true,
+                        close_code: CloseCode::UnsupportedData,
+                        close_message: "Received unexpected message.".to_string(),
+                    }),
                 }
             })
         })
         .await;
     }
 
-    /// Upgrade HTTP connection to websocket:
+    /// Upgrade HTTP connection to WebSocket
     async fn upgrade(
         ws: WebSocketUpgrade<ServerMsg, ClientMsg>,
         shutdown: broadcast::Sender<()>,
     ) -> impl IntoResponse {
         let shutdown_rx = shutdown.subscribe();
-        debug!("Websocket upgrade request received.");
+        debug!("WebSocket upgrade request received.");
         ws.on_upgrade(move |socket| websocket(socket, shutdown_rx))
     }
+
     route(
         "/command_execute/",
         get(move |ws: WebSocketUpgrade<_, _>| upgrade(ws, shutdown.clone())),
