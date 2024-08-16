3f091ca069042511b113a5d66fdee45841d78936
EnigmaCurry <ryan@enigmacurry.com>
2024-08-16 16:19:21 -0600

    retain messages

--- frontend/src/components/terminal.rs
+++ frontend/src/components/terminal.rs
@@ -1,7 +1,7 @@
-use gloo::console::debug;
-//use patternfly_yew::prelude::*;
+use crate::random::generate_random_string;
 use crate::{pages::workstation::WorkstationTab, websocket::setup_websocket};
 use dry_console_dto::websocket::ServerMsg;
+use gloo::console::debug;
 use std::cell::RefCell;
 use std::rc::Rc;
 use wasm_bindgen::closure::Closure;
@@ -14,57 +14,65 @@ pub struct TerminalOutputProps {
     pub selected_tab: WorkstationTab,
 }
 
-pub struct TerminalOutputState {
-    ws: Option<Rc<RefCell<WebSocket>>>,
-    _callback: Option<Closure<dyn FnMut(web_sys::MessageEvent)>>,
+enum MsgAction {
+    AddMessage(String),
 }
 
-impl Drop for TerminalOutputState {
-    fn drop(&mut self) {
-        debug!("Drop!");
-        if let Some(ws) = &self.ws {
-            ws.borrow().close().ok(); // Properly close the WebSocket
+struct MessagesState {
+    messages: Vec<String>,
+}
+
+impl Reducible for MessagesState {
+    type Action = MsgAction;
+
+    fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
+        match action {
+            MsgAction::AddMessage(new_message) => {
+                let mut messages = self.messages.clone();
+                messages.push(new_message);
+                MessagesState { messages }.into()
+            }
         }
     }
 }
 
 #[function_component(TerminalOutput)]
 pub fn terminal_output(props: &TerminalOutputProps) -> Html {
-    let messages = use_state_eq(Vec::new);
+    let messages = use_reducer(|| MessagesState {
+        messages: Vec::new(),
+    });
     let ws_state = use_state(|| None);
     let callback_state = use_state(|| None);
     let is_connected = use_state(|| false); // Track connection status
 
     {
-        let messages = messages.clone();
         let selected_tab = props.selected_tab.clone();
         let ws_state_clone = ws_state.clone();
         let callback_state_clone = callback_state.clone();
         let is_connected_clone = is_connected.clone();
+        let messages_clone = messages.clone();
 
-        use_effect_with(selected_tab,move |selected_tab| {
+        use_effect_with(selected_tab, move |selected_tab| {
             if *selected_tab == WorkstationTab::DRymcgTech && !*is_connected_clone {
+                let messages_ref = messages_clone.clone();
                 let on_message = Callback::from(move |server_msg: ServerMsg| {
-                    messages.set({
-                        let mut new_messages = (*messages).clone();
-                        new_messages.push(format!("{:?}", server_msg));
-                        new_messages
-                    });
+                    let new_message = format!("{:?} {}", server_msg, generate_random_string(5));
+                    messages_ref.dispatch(MsgAction::AddMessage(new_message));
                 });
-        
+
                 debug!("setup_websocket");
                 let setup = setup_websocket("/api/workstation/command_execute/", on_message);
-        
+
                 ws_state_clone.set(Some(Rc::new(RefCell::new(setup.socket))));
                 callback_state_clone.set(Some(setup.on_message_closure));
                 is_connected_clone.set(true); // Mark as connected
             }
-        
+
             move || {
                 if *is_connected_clone {
                     if let Some(ws_rc) = &*ws_state_clone {
-                        let ws_ref = ws_rc.borrow();
-                        ws_ref.borrow().close().ok();
+                        let ws = ws_rc.borrow();
+                        ws.borrow().close().ok(); // Properly close the WebSocket
                     }
                     ws_state_clone.set(None);
                     callback_state_clone.set(None);
@@ -76,7 +84,7 @@ pub fn terminal_output(props: &TerminalOutputProps) -> Html {
 
     html! {
         <div>
-            { for messages.iter().map(|message| html!{ <p>{message}</p> }) }
+            { for messages.messages.iter().map(|message| html!{ <p>{message}</p> }) }
         </div>
     }
 }
